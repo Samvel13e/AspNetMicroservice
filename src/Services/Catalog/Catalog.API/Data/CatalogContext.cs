e271bf2ad5b12513bc1de88def0211ea41539c69
Samvel Yeghikyan <Samvel.Yeghikyan@technamin.com>
2023-12-23 18:18:34 +0400

    Add CatalogController

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration cofiguration)
+        {
+            var client = new MongoClient(cofiguration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(cofiguration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(cofiguration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
